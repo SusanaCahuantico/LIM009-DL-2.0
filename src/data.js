d4736474b79b9cbf488249c553edf39d4bb36ddd
susanita12352 <scahuantico@gmail.com>
2019-04-19 17:05:21 -0500

    Creacion de mi funcion recorrerData y el pintado en pantalla

--- src/data.js
+++ src/data.js
@@ -1,25 +1,41 @@
 /* Manejo de data */
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
+// const personajes = document.getElementById('personajes');
 
-const json = () => {
+const cargarJSON = () => {
     fetch('https://raw.githubusercontent.com/SusanaCahuantico/LIM009-DL-2.0/master/src/data/potter.json')
     .then( res => res.json() )
     .then( datos => {
-       pintar(datos)
+        recorrerData(datos)
     })
 }
-json();
+cargarJSON();
 
-const pintar = (datos) => {
+// Creando función lista de personajes de Harry Potter:
+const recorrerData = (datos) => {
+    let arrLista = [];
     for(let i=0; i<datos.length; i++){
-        // eslint-disable-next-line no-console
-        console.log(datos[i].name)
-        // eslint-disable-next-line no-console
-        console.log(datos[i].species)
-    }
+        arrLista.push({
+            image: datos[i].image,
+            name: datos[i].name,
+            house: datos[i].house,
+            species: datos[i].species,
+            hogwartsStaff: datos[i].hogwartsStaff,
+            hogwartsStudent: datos[i].hogwartsStudent,
+            yearOfBirth: datos[i].yearOfBirth,
+            actor: datos[i].actor
+        });
+    } 
+   // console.log(arrLista);
+    return(arrLista);
 }
 
+/* Creando mi función de orden ascendente 
+const orderAsc = (datos) => {
+    let
+} */
 window.potter = {
-    json,
+    cargarJSON,
+    recorrerData
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,36 @@
-/* Manejo del DOM  
-const paintData = document.getElementById('paintData');
-paintData.addEventListener('click', mostrarData);
-*/
+/* Manejo del DOM */
+// const allData = 'https://raw.githubusercontent.com/SusanaCahuantico/LIM009-DL-2.0/master/src/data/potter.json';
+
+const personajes = document.getElementById('personajes');
+
+const cardJSON = () => {
+    fetch('https://raw.githubusercontent.com/SusanaCahuantico/LIM009-DL-2.0/master/src/data/potter.json')
+    .then( res => res.json() )
+    .then( datos => {
+        listaPotter(datos)
+    })
+}
+ cardJSON();
+
+/* imprimir la lista de los personajes: */
+
+const listaPotter = (datos) => {
+    let mostrar = '';
+    for (let i=0; i<datos.length ; i++) {
+       let box = `
+       <div>
+       <figure>
+       <img src="${ datos[i].image}"/>
+       </figure>
+       <p> Nombre: ${ datos[i].name} </p>
+       <p> Casa: ${ datos[i].house} </p>
+       <p> Especie:${ datos[i].species} </p>
+       <p> Actor: ${ datos[i].actor} </p>
+       </div>
+       `;
+       mostrar += box;
+    }
+   personajes.innerHTML = mostrar;
+};
+
+listaPotter();
